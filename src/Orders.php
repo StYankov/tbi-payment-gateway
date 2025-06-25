2be6709fd4fc144e80f9fd68759c055f38342638
StYankov <st.yankov00@gmail.com>
2025-06-25 22:12:49 +0300

    fix: add extended hold period for tbi-loan-fixed

--- src/Orders.php
+++ src/Orders.php
@@ -9,7 +9,7 @@ class Orders {
         add_filter( 'woocommerce_cancel_unpaid_order', [ $this, 'tbi_loan_extended_hold_period' ], 10, 2 );
     }
     public function tbi_loan_extended_hold_period( bool $cancel, WC_Order $order ) {
-        if( $order->get_payment_method() !== 'tbi-bnpl' && $order->get_payment_method() !== 'tbi-loan' ) {
+        if( $order->get_payment_method() !== 'tbi-bnpl' && $order->get_payment_method() !== 'tbi-loan' && $order->get_payment_method() !== 'tbi-loan-fixed' ) {
             return $cancel;
         }
 
